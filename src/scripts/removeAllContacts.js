cd78bb2ba38d0c1aeebea7d4432231788cbdf957
belzevol <belzevol@gmail.com>
2025-03-13 18:16:40 +0200

    init

--- src/scripts/removeAllContacts.js
+++
@@ -1,8 +0,0 @@
-import * as fs from 'node:fs/promises';
-import { PATH_DB } from '../constants/contacts.js';
-
-export const removeAllContacts = async () => {
-  await fs.writeFile(PATH_DB, JSON.stringify([], null, 2));
-};
-
-removeAllContacts();
--- src/utils/readContacts.js
+++
@@ -1,7 +0,0 @@
-import * as fs from 'node:fs/promises';
-import { PATH_DB } from '../constants/contacts.js';
-
-export const readContacts = async () => {
-  const contacts = await fs.readFile(PATH_DB);
-  return contacts ? JSON.parse(contacts) : [];
-};
