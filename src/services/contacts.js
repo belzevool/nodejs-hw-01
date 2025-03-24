6d811fb5559c54f18796eef1367d9829dfbed2d6
belzevol <belzevol@gmail.com>
2025-03-24 14:42:39 +0200

    fin

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -7,3 +7,25 @@ export const getContacts = async () => {
 export const getContactById = async (contactId) => {
   return await ContactsCollection.findById(contactId);
 };
+
+export const createContact = async (payload) => {
+  return await ContactsCollection.create(payload);
+};
+
+export const deleteContact = async (contactId) => {
+  return await ContactsCollection.findOneAndDelete({
+    _id: contactId,
+  });
+};
+
+export const updateContact = async (contactId, payload) => {
+  const result = await ContactsCollection.findOneAndUpdate(
+    { _id: contactId },
+    payload,
+    {
+      new: true,
+      includeResultMetadata: true,
+    },
+  );
+  return result.value;
+};
